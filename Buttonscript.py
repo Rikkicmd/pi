47e959b4701e37844b8bf82bf9bce9b80dc20283
Superturtle-2019 <rikki1@hotmail.co.uk>
2020-02-29 12:32:14 +0000

    Add files via upload

--- Buttonscript.py
+++ Buttonscript.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python
-#          Uses GPIO5  (BOARD Pin 29)
+#     Pin 10 is input and initial value is low (OFF)
 
 
-import RPi.GPIO as gpio
-from subprocess import call
-import time
+import RPi.GPIO as GPIO
 import os
 
-gpio.setmode(gpio.BCM)
-gpio.setup(5, gpio.IN, pull_up_down = gpio.PUD_UP)
-gpio.add_event_detect(5, gpio.FALLING, os.system("ping 8.8.8.8 -c 5"), bouncetime=300)
+
+def button_callback(channel):
+    print("Starting Update!")
+    os.system ("ping 8.8.8.8 -C 5")
+
+GPIO.setwarnings (False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
+
 
 
 exit()
